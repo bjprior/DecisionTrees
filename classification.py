b1b236c4feb8b5101f75e3de8d4530d934869a01
bjp19 <be1994JP>
2020-01-28 13:42:00 +0000

    Added implementations for DecisionTreeClassifier class

--- classification.py
+++ classification.py
@@ -8,7 +8,78 @@
 ##############################################################################
 
 import numpy as np
+import dataReader as dr
 
+class LeafNode(object):
+    """
+    A leafNode
+
+    Attributes
+    ----------
+    A letter which contains the majority label
+
+    """
+
+    def __init__(self, letter = " "):
+        self.letter = letter
+
+
+class Node(object):
+    """
+    A Node
+
+    Attributes
+    ----------
+    split_col: int
+        The column index referencing the attribute on which the data set will be split
+
+    threshold: int
+        The threshold value on which the data will be split for the attribute given
+
+    """
+    def __init__(self, col, thres):
+        self.split_col = col
+        self.threshold = thres
+
+def InduceDecisionTree(x,y):
+
+    letters = ["A","C","E","G","O","Q"]
+    count = np.zeros(6)
+
+    #Check for multiple letters in data set remaining
+
+    for i in range(len(y)):
+        for l in range(len(letters)):
+            if(letters[l] == chr(y[i])):
+                count[l] +=1
+    count_max = 0
+    index = 10
+
+    # Get max count and index position
+    for l in range(0,6):
+        if count[l] > max:
+            max = count[l]
+            index = l
+
+    #Check if all samples have same label also checks for 1 remaining
+    if np.nonzero(count_max) == 1:
+        return LeafNode(letters[index])
+    else:
+        Node_new = Node(FindBestNode(x,y))
+
+
+def SplitDataset(attribute, value, dataset):
+
+    left = list()
+    right = list()
+
+    for row in dataset:
+        if row[attribute] <= value:
+            left.append(row)
+        else:
+            right.append(row)
+
+    return left,right
 
 class DecisionTreeClassifier(object):
     """
@@ -59,7 +130,9 @@ class DecisionTreeClassifier(object):
         #######################################################################
         #                 ** TASK 2.1: COMPLETE THIS METHOD **
         #######################################################################
-        
+
+        InduceDecisionTree(x,y)
+
         
         
         # set a flag so that we know that the classifier has been trained
@@ -104,3 +177,13 @@ class DecisionTreeClassifier(object):
         return predictions
         
 
+if __name__ == "__main__":
+    data = dr.parseFile("data/toy.txt")
+    xs = np.hsplit(data,4)
+    y = xs[3]
+    x = np.hstack((xs[0],xs[1],xs[2]))
+
+    Tree = DecisionTreeClassifier
+
+    Tree.__init__(Tree)
+    Tree.train(Tree,x,y)

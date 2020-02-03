3e4d99bb775ff016bc5a257669366e0782960878
bjp19 <benjamin.prior19@imperial.ac.uk>
2020-02-03 18:52:36 +0000

    BP - Updated crossValidation.py to include standard deviation of the 10 fold cross Validation

--- crossValidation.py
+++ crossValidation.py
@@ -2,23 +2,30 @@ import numpy as np
 import dataReader as dr
 import classification as cls
 import eval as ev
-import math
-
 
 def k_fold_cross_validation(data_set, k):
-    errors = np.zeros(k)
-    tree = cls.DecisionTreeClassifier()
 
-    for i in range(1, k):
+    accuracy = np.zeros(k)
+    tree = cls.DecisionTreeClassifier()
+    best_tree = cls.DecisionTreeClassifier()
+    max_accuracy = 0
+
+    for i in range(1, k+1):
         testing, training = split_set(data_set, k, i)
         training_x, training_y = training[:, :-1], training.T[-1]
         tree.train(training_x, training_y)
         predictions = tree.predict(testing)
         eval = ev.Evaluator()
-        confusion = eval.confusion_matrix(predictions, training_y)
-        errors[i - 1] = eval.accuracy(confusion)
+        testing_y = testing.T[-1]
+        print("Prediction(" + str(i) + "): " + str(predictions))
+        print("Label(" + str(i) + ")" + str(testing_y))
+        confusion = eval.confusion_matrix(predictions, testing_y)
+        accuracy[i - 1] = eval.accuracy(confusion)
+        if accuracy[i-1] > max_accuracy:
+            best_tree = tree
+        print("Accuracy(" + str(i) + "):" + str(accuracy[i-1]))
 
-    return np.mean(errors)
+    return accuracy, best_tree
 
 
 def split_set(data_set, k, fold):
@@ -30,25 +37,33 @@ def split_set(data_set, k, fold):
         return
     else:
         width = len(data_set[0])
-        data_splits = np.split(data_set,k)
-        #np.random.shuffle(data_set) <- randomises the set
+        data_splits = np.split(data_set, k)
         training_set = np.empty(shape=[0, width], dtype=int)
 
         for i in range(len(data_splits)):
             if i == fold - 1:
                 testing_set = np.array(data_splits[i])
             else:
-                training_set = np.concatenate((training_set,data_splits[i]),axis=0)
+                training_set = np.concatenate((training_set, data_splits[i]), axis=0)
 
         training_set = np.asarray(training_set)
 
         return testing_set, training_set
 
+def standard_dev(accuracy,k,n):
+
+    errors = np.ones(k) - accuracy
+    std_dev = np.sqrt((errors*accuracy)/n)
+
+    print(std_dev)
+
 
 if __name__ == "__main__":
-    data = dr.parseFile("data/toy.txt")
-    n = len(data)
+    data = dr.parseFile("data/train_sub.txt")
     k = 10
-    average_accuracy = k_fold_cross_validation(data, k)
-    #err = 1 - average_accuracy
-    #standard_deviation = math.sqrt((err*(1-err))/n)
+    n = len(data)/k
+    accuracy, tree = k_fold_cross_validation(data, k)
+    #accuracy = [0.3333333, 0.533333, 0.7166666666, 0.383333333333, 0.4166666666, 0.48333333333, 0.48333333333
+    #           , 0.36666666666666664, 0.5333333333333333, 0.43333333333333335]
+    standard_dev(accuracy, k, n)
+
